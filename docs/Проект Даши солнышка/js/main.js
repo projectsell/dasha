309647115f509b58b0d01871c1b8a5e1af73c443
projectsell <120035516+projectsell@users.noreply.github.com>
2023-03-21 01:33:10 +0300

    Delete docs/Проект Даши солнышка directory

--- docs/Проект Даши солнышка/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.katalog__acc-title').on('click', function(e){
-        e.preventDefault()
-        $(this).next().slideToggle()
-        $(this).toggleClass('katalog__acc-title--active')
-        
-    })
-})
